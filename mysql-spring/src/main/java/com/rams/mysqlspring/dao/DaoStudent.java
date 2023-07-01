c384c1320605884ff54547d40613e02c177a087c
Ricardo Manzano <ricardoamsrams@gmail.com>
2023-07-01 19:44:20 -0400

    Fix #2 requirement. Create interface for Student class

---
+++ mysql-spring/src/main/java/com/rams/mysqlspring/dao/DaoStudent.java
@@ -0,0 +1,15 @@
+package com.rams.mysqlspring.dao;
+
+import com.rams.mysqlspring.entity.StudentEntity;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DaoStudent {
+
+
+
+
+}
